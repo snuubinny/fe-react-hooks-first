960b78d7b246f6b329465e072c90b8c44595f6ac
snubin <jeonsubin5156@inha.edu>
2024-04-15 16:47:17 +0900

    complete

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+
+import React, { useState,useEffect } from 'react'
 
 const Name = () => {
+
+  const[name,setname]=useState('');
+  const[nickname,setnickname]=useState('')
+  
+  const handleName=e=>{
+    setname(e.target.value)
+  }
+  const handlenickname=e=>{
+    setnickname(e.target.value)
+  }
+
   return (
     <div className="name">
       <h>Name</h>
       <hr></hr>
-      <input name="myName" placeholder="이름을 입력해주세요" />
-      <input name="nickName" placeholder="닉네임을 입력해주세요" />
-      <p>이름</p>
-      <p>닉네임</p>
+      <input name="myName" placeholder="이름을 입력해주세요" 
+      type='text'value={name}onChange={handleName}/>
+      <input name="nickName" placeholder="닉네임을 입력해주세요"
+      type='text'value={nickname}onChange={handlenickname} />
+      <p>이름:{name===''? '아직 이름이 없습니다.':name}</p>
+      <p>닉네임:{nickname===''? '아직 닉네임이 없습니다.':nickname}</p>
     </div>
   )
 }
